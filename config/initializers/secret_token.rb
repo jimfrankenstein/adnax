78dfaf836adecc639ca14dea0c4292b1e5451670
Brian <brian.reed.design@gmail.com>
2014-05-01 19:18:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adnax::Application.config.secret_token = 'c3e2b0fc55b73dcfd5f86847f4a95a0b8c9fe6825fedc7be30856952d804cb15bb61de47ece818e7898cb4674eaee3c79c196c770d3121697b9cfdc3ebb59a99'
